5bb91ad8e21fedd8683dd2a1ac00e391126d2997
GaryNg <garyng.zhongbo@gmail.com>
2019-01-15 02:43:12 +0800

    feat: add Book, Rating and User models

--- src/api/app/Book.php
+++ src/api/app/Book.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    //
+    protected $fillable = ['user_id', 'title', 'description'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function ratings()
+    {
+        return $this->hasMany(Rating::class);
+    }
 }
