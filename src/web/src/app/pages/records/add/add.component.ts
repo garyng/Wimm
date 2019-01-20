5f41fe584ef4d6ca54f19e3e5d3d158328a5edec
GaryNg <garyng.zhongbo@gmail.com>
2019-01-20 16:15:28 +0800

    feat(web): add record

--- src/web/src/app/pages/records/add/add.component.ts
+++ src/web/src/app/pages/records/add/add.component.ts
@@ -1,10 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { NbDateService } from '@nebular/theme';
+import { CurrenciesRepository, UserRepository, CategoriesRepository, RecordsRepository } from 'src/app/@services/repository-base';
+import { tap, flatMap } from 'rxjs/operators';
+import { Currency } from 'src/app/@models/currency';
+import { Category } from 'src/app/@models/category';
+import { ReplaySubject } from 'rxjs';
+import { ErrorsHandler } from 'src/app/@services/errors-handler';
+import { SpinnerService } from 'src/app/@services/spinner.service';
+import { Record } from 'src/app/@models/record';
+import { swalAdd, swalAdded } from 'src/app/@common/swal-mixins';
+import { SwalService } from 'src/app/@services/swal.service';
 
 enum RecordTypes {
   expense = 'expense',
   income = 'income'
 }
+
 
 @Component({
   selector: 'app-add',
@@ -12,35 +23,97 @@ enum RecordTypes {
   styleUrls: ['./add.component.scss']
 })
 export class AddComponent implements OnInit {
-
   recordType: keyof typeof RecordTypes = 'expense';
   recordTypes = RecordTypes;
 
-  currencies: string[] = [
-    'MYR',
-    'SGD',
-    'USD',
-    'BTC',
-    'BTC'
-  ];
-  selectedCurrency = this.currencies[0];
-  defaultCurrency = 'MYR';
+  currencies: Currency[];
+  selectedCurrency: string;
+  defaultCurrency: string;
 
-  categories: string[] = [
-    'Food',
-    'Rental',
-    'Bill',
-  ];
-  selectedCategory = this.categories[0];
+  categories: Category[];
+  selectedCategory: Category;
 
   selectedDate: Date;
-  description = "";
+  description = '';
+  amount = 0;
 
-  constructor(dateService: NbDateService<Date>) {
-    this.selectedDate = dateService.today();
+  load$: ReplaySubject<{}> = new ReplaySubject();
+
+  constructor(
+    private dateService: NbDateService<Date>,
+    private currenciesRepo: CurrenciesRepository,
+    private categoriesRepo: CategoriesRepository,
+    private userRepo: UserRepository,
+    private recordsRepo: RecordsRepository,
+    private onError: ErrorsHandler,
+    public spinner: SpinnerService,
+    private swalService: SwalService
+  ) {
+    this.reset();
+    this.load$
+      .pipe(
+        tap(_ => this.spinner.show()),
+        flatMap(_ => this.userRepo.get()),
+        tap(user => {
+          this.defaultCurrency = user.currency;
+          this.selectedCurrency = user.currency;
+        }),
+        flatMap(_ => this.currenciesRepo.getAll()),
+        tap(currencies => (this.currencies = currencies)),
+        flatMap(_ => this.categoriesRepo.getAll()),
+        tap(categories => {
+          this.categories = categories;
+          this.selectedCategory = categories[0];
+        })
+      )
+      .subscribe(
+        _ => {
+          this.spinner.hide();
+        },
+        error => {
+          this.onError.notify(error);
+          this.spinner.hide();
+        }
+      );
+
+    this.load$.next();
   }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  onAdd() {
+    let normalizedAmount = Math.abs(this.amount);
+    if (this.recordType === RecordTypes.expense) {
+      normalizedAmount *= -1;
+    }
+    const record = Record.create({
+      amount: normalizedAmount,
+      description: this.description,
+      currency: this.selectedCurrency,
+      categoryId: this.selectedCategory && this.selectedCategory.id,
+      timestamp: this.selectedDate.valueOf(), // milliseconds
+      id: 0,
+      category: null,
+      userId: 0,
+      createdAt: null,
+      updatedAt: null,
+      rateToLocal: 0
+    });
+    this.swalService.fire(
+      swalAdd.mixin({
+        text: 'Add a new record?'
+      }),
+      this.recordsRepo.add(record),
+      _ => this.reset(),
+      _ => { },
+      () => swalAdded.fire({})
+    );
   }
 
+  reset() {
+    this.amount = 0;
+    this.description = '';
+    this.selectedDate = this.dateService.today();
+    this.recordType = RecordTypes.expense;
+  }
 }
