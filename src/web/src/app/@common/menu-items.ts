4c7a8f8d1e58c27af7c9468cca30f474026d122b
GaryNg <garyng.zhongbo@gmail.com>
2019-01-20 19:10:38 +0800

    fix(web): add menu items

--- src/web/src/app/@common/menu-items.ts
+++ src/web/src/app/@common/menu-items.ts
@@ -2,15 +2,23 @@ import { NbMenuItem } from '@nebular/theme';
 
 export const MENU_ITEM: NbMenuItem[] = [
   {
-    title: 'Records',
-    icon: 'nb-list',
-    link: '/app/records',
+    title: 'Dashboard',
+    icon: 'nb-home',
+    link: '/app/dashboard',
     home: true
   },
   {
-    title: 'Dashboard',
-    icon: 'nb-list',
-    link: '/app/dashboard',
-    home: true
+    title: 'Records',
+    icon: 'nb-compose',
+    children: [
+      {
+        title: 'All Records',
+        link: '/app/records/list',
+      },
+      {
+        title: 'New Record',
+        link: '/app/records/add',
+      }
+    ]
   },
 ];
