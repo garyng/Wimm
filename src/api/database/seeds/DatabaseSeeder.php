9ca62399b4a1cb3d8349d73dcbaa732b2381355d
GaryNg <garyng.zhongbo@gmail.com>
2019-01-24 19:20:06 +0800

    fix(api): disable foreign key check when seeding database

--- src/api/database/seeds/DatabaseSeeder.php
+++ src/api/database/seeds/DatabaseSeeder.php
@@ -12,11 +12,13 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // order matters
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
         $this->call(UsersTableSeeder::class);
         $this->call(CategoriesTableSeeder::class);
         $this->call(RecordsTableSeeder::class);
         $this->call(RecurrencesTableSeeder::class);
         $this->call(BudgetsTableSeeder::class);
         $this->call(CurrenciesTableSeeder::class);
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
 }
